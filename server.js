f9799f51b8e2b38dd48d33a5515934b62ed71839
abu-hammad07 <hammadkamal2003@gmail.com>
2025-09-28 22:43:46 +0500

    port changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const apiKey = process.env.ELEVENLABS_API_KEY;
 const agentId = process.env.AGENT_ID;
 
